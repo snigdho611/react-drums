bc5213a950082236eb33564b303377a4531d2909
Snigdho Dip Howlader <snigdho.howlader@gmail.com>
2023-07-06 11:21:33 +0600

    Adding conditional events for both click and key press

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,26 @@ import tom from "./sounds/tom.wav";
 import tink from "./sounds/tink.wav";
 
 function App() {
-  const clapAudio = new Audio(clap);
-  const hihatAudio = new Audio(hihat);
-  const kickAudio = new Audio(kick);
-  const openhatAudio = new Audio(openhat);
-  const boomAudio = new Audio(boom);
-  const rideAudio = new Audio(ride);
-  const snareAudio = new Audio(snare);
-  const tomAudio = new Audio(tom);
-  const tinkAudio = new Audio(tink);
+  // const clapAudio = new Audio(clap);
+  // const hihatAudio = new Audio(hihat);
+  // const kickAudio = new Audio(kick);
+  // const openhatAudio = new Audio(openhat);
+  // const boomAudio = new Audio(boom);
+  // const rideAudio = new Audio(ride);
+  // const snareAudio = new Audio(snare);
+  // const tomAudio = new Audio(tom);
+  // const tinkAudio = new Audio(tink);
 
-  const clapRef = useRef<HTMLButtonElement|null>(null)
-  const hihatRef = useRef<HTMLButtonElement | null>(null)
-  const kickRef = useRef<HTMLButtonElement | null>(null)
-  const openhatRef = useRef<HTMLButtonElement | null>(null)
-  const boomRef = useRef<HTMLButtonElement | null>(null)
-  const rideRef = useRef<HTMLButtonElement | null>(null)
-  const snareRef = useRef<HTMLButtonElement | null>(null)
-  const tomRef = useRef<HTMLButtonElement | null>(null)
-  const tinkRef = useRef<HTMLButtonElement | null>(null)
-  
+  const clapRef = useRef<HTMLButtonElement | null>(null);
+  const hihatRef = useRef<HTMLButtonElement | null>(null);
+  const kickRef = useRef<HTMLButtonElement | null>(null);
+  const openhatRef = useRef<HTMLButtonElement | null>(null);
+  const boomRef = useRef<HTMLButtonElement | null>(null);
+  const rideRef = useRef<HTMLButtonElement | null>(null);
+  const snareRef = useRef<HTMLButtonElement | null>(null);
+  const tomRef = useRef<HTMLButtonElement | null>(null);
+  const tinkRef = useRef<HTMLButtonElement | null>(null);
+
   useMemo(() => {
     const clapAudio = new Audio(clap);
     const hihatAudio = new Audio(hihat);
@@ -41,61 +41,121 @@ function App() {
     const snareAudio = new Audio(snare);
     const tomAudio = new Audio(tom);
     const tinkAudio = new Audio(tink);
+
     const handleUserKeyPress = (event: KeyboardEvent) => {
       if (event.key === "a" || event.key === "A") {
-        // clapAudio.play();
-        clapRef.current?.click()
+        clapAudio.currentTime = 0;
+        clapAudio.play();
       }
       if (event.key === "s" || event.key === "S") {
-        // hihatAudio.play();
-        hihatRef.current?.click()
+        hihatAudio.currentTime = 0;
+        hihatAudio.play();
       }
       if (event.key === "d" || event.key === "D") {
-        // kickAudio.play();
-        kickRef.current?.click()
+        kickAudio.currentTime = 0;
+        kickAudio.play();
       }
       if (event.key === "f" || event.key === "F") {
-        // openhatAudio.play();
-        openhatRef.current?.click()
+        openhatAudio.play();
       }
       if (event.key === "g" || event.key === "G") {
-        // boomAudio.play();
-        boomRef.current?.click()
+        boomAudio.currentTime = 0;
+        boomAudio.play();
       }
       if (event.key === "h" || event.key === "H") {
-        // rideAudio.play();
-        rideRef.current?.click()
+        rideAudio.currentTime = 0;
+        rideAudio.play();
       }
       if (event.key === "j" || event.key === "J") {
-        // snareAudio.play();
-        snareRef.current?.click()
+        snareAudio.currentTime = 0;
+        snareAudio.play();
       }
       if (event.key === "k" || event.key === "K") {
-        // tomAudio.play();
-        tomRef.current?.click()
+        tomAudio.currentTime = 0;
+        tomAudio.play();
       }
       if (event.key === "l" || event.key === "L") {
-        // tinkAudio.play();
-        tinkRef.current?.click()
+        tinkAudio.currentTime = 0;
+        tinkAudio.play();
       }
     };
+
+    const handleMouseClick = (event: MouseEvent) => {
+      if (clapRef.current) {
+        clapRef.current.addEventListener("click", () => {
+          clapAudio.currentTime = 0;
+          clapAudio.play();
+        });
+      }
+      if (hihatRef.current) {
+        hihatRef.current.addEventListener("click", () => {
+          clapAudio.currentTime = 0;
+          clapAudio.play();
+        });
+      }
+      if (hihatRef.current) {
+        hihatRef.current.addEventListener("click", () => {
+          hihatAudio.currentTime = 0;
+          hihatAudio.play();
+        });
+      }
+      if (openhatRef.current) {
+        openhatRef.current.addEventListener("click", () => {
+          openhatAudio.currentTime = 0;
+          openhatAudio.play();
+        });
+      }
+      if (boomRef.current) {
+        boomRef.current.addEventListener("click", () => {
+          openhatAudio.currentTime = 0;
+          openhatAudio.play();
+        });
+      }
+      if (rideRef.current) {
+        rideRef.current.addEventListener("click", () => {
+          rideAudio.currentTime = 0;
+          rideAudio.play();
+        });
+      }
+      if (snareRef.current) {
+        snareRef.current.addEventListener("click", () => {
+          snareAudio.currentTime = 0;
+          snareAudio.play();
+        });
+      }
+      if (tomRef.current) {
+        tomRef.current.addEventListener("click", () => {
+          tomAudio.currentTime = 0;
+          tomAudio.play();
+        });
+      }
+      if (tinkRef.current) {
+        tinkRef.current.addEventListener("click", () => {
+          tinkAudio.currentTime = 0;
+          tinkAudio.play();
+        });
+      }
+    };
+
     document.addEventListener("keydown", handleUserKeyPress);
+    document.addEventListener("mousedown", handleMouseClick);
     return () => {
       window.removeEventListener("keydown", handleUserKeyPress);
+      window.removeEventListener("mousedown", handleMouseClick);
     };
   }, []);
 
   return (
     <div className="App">
-      <button ref={clapRef} onClick={()=>{clapAudio.play()}}>clap</button>
-      <button ref={hihatRef} onClick={()=>{hihatAudio.play()}}>hihat</button>
-      <button ref={kickRef} onClick={()=>{kickAudio.play()}}>kick</button>
-      <button ref={openhatRef} onClick={()=>{openhatAudio.play()}}>openhat</button>
-      <button ref={boomRef} onClick={()=>{boomAudio.play()}}>boom</button>
-      <button ref={rideRef} onClick={()=>{rideAudio.play()}}>ride</button>
-      <button ref={snareRef} onClick={()=>{snareAudio.play()}}>snare</button>
-      <button ref={tomRef} onClick={()=>{tomAudio.play()}}>tom</button>
-      <button ref={tinkRef} onClick={()=>{tinkAudio.play()}}>tink</button>
+      <button ref={clapRef}>clap</button>
+      <button ref={hihatRef}>hihat</button>
+      <button ref={kickRef}>kick</button>
+      <button ref={openhatRef}>openhat</button>
+      <button ref={boomRef}>boom</button>
+      <button ref={rideRef}>ride</button>
+      <button ref={snareRef}>snare</button>
+      <button ref={tomRef}>tom</button>
+      <button ref={tinkRef}>tink</button>
     </div>
   );
 }
